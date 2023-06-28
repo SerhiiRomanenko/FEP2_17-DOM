61407dc6a4798e5c78c65f728e470cc27457e270
Serhii <SerhiiRomanenko13@gmail.com>
2023-06-28 14:14:04 +0300

    HW_17_DOM

--- main.js
+++ main.js
@@ -23,6 +23,29 @@ function generateList(array) {
 }
 
 
+//    Інший варіант даної функції
+
+
+// function generateList(array) {
+//   let ul = document.createElement("ul");
+//
+//   for (let item of array) {
+//     if (!Array.isArray(item)) {
+//       let li = document.createElement("li");
+//       li.textContent = item;
+//       ul.append(li);
+//     } else {
+//       let innerUl = document.createElement("li");
+//       ul.append(innerUl);
+//       innerUl.append(generateList(item));
+//     }
+//   }
+//
+//   document.body.append(ul);
+//   return ul;
+// }
+
+
 generateList([1, 2, 3]);
-generateList( [1,2, [1.1,1.2,1.3], 3]);
+generateList([1, 2, [1.1, 1.2, 1.3, [5, 6, 7]], 3]);
 generateList([1, 2, [1.1, 1.2, 1.3, [3, 4, 5]], 3]);
